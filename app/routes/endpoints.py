699ba8b612cae03e4c92187b4deeb4e617ab8115
Osmolovsky K. <kirillosm09@gmail.com>
2026-02-13 18:15:12 +1000

    initial commit

---
+++ app/routes/endpoints.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/{alias_url}")
+async def redirect_from_alias_to_url():
+    """Steps:
+        1. Get url from cache
+        2. If there is no value in cache, turn alias_url to ID and search in database. Usually, alias_url is base62 of ID, but it may be custom alias
+        3. If there is no value in database, return 404
+        4. Write extracted value to cache and return redirect 302
+    """
+    pass
+
+@router.post("/create")
+async def create_short_link(url: str, server_url: str, custom_alias: str | None = None):
+    """Steps:
+        1. Generate unique ID (snowflake)
+        2. Turn ID to base62
+        3. Save (ID, url, custom_alias) to DB
+        4. If custom_alias specified, concatinate it with server_url and return. If not, add base62 ID to server_url and return it
+    """
+    pass
