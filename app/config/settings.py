03c2dcab40c5afd3b568ef8d340494f9bd4deca7
Osmolovsky K. <kirillosm09@gmail.com>
2026-02-17 15:12:01 +1000

    utils: remove global pod_name

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,4 +1,6 @@
 import os
 
-
+"""
+commentary: now generator reads POD_NAME itself
 pod_name = os.getenv("POD_NAME") #used by snowflake generator
+"""
--- app/utils/id_from_pod_name.py
+++ app/utils/id_from_pod_name.py
@@ -1,10 +1,11 @@
 import hashlib
 import os
 
-from app.config.settings import pod_name
-
 
 def get_node_id_from_pod_name() -> int:
+    pod_name = os.environ.get("POD_NAME", "")
+    if not pod_name:
+        raise RuntimeError("POD_NAME environment variable is not set")
     hash_bytes = hashlib.sha256(pod_name.encode()).digest()
     hash_int = int.from_bytes(hash_bytes[:2], byteorder='big')
     return hash_int % 1024
