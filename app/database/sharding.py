1b8b1d7ae439b1aba725178cb5bd7fed4f610aa9
Osmolovsky K. <kirillosm09@gmail.com>
2026-02-18 13:36:19 +1000

    database: add missing imports, remove execute_query to avoid complex lambdas

--- app/database/sharding.py
+++ app/database/sharding.py
@@ -1,7 +1,13 @@
+from contextlib import contextmanager
+from typing import Generator
+from sqlmodel import create_engine, Session
+from sqlalchemy.orm import sessionmaker
+
 from app.utils.hash_ring import HashRing
 
 
 class DatabaseManager:
+
     def __init__(self, db_configs: dict):
         """Пример db_configs: db_configs = {
             'db1': 'postgresql://user:pass@host1/dbname',
@@ -11,27 +17,40 @@ class DatabaseManager:
         """
         self.hash_ring = HashRing(list(db_configs.keys()))
         self.engines = {name: create_engine(config) for name, config in db_configs.items()}
-        self.sessions = {}
+        self.session_factories = {}
+        for name in self.engines:
+            self.session_factories[name] = sessionmaker(
+                bind=self.engines[name],
+                class_=Session,
+                autocommit=False,
+                autoflush=False
+            )
 
-    def get_session(self, shard_key: str):
-        node = self.hash_ring.get_node(shard_key)
-        if node not in self.sessions:
-            Session = sessionmaker(bind=self.engines[node])
-            self.sessions[node] = Session()
-        return self.sessions[node]
 
-    def execute_query(self, shard_key, query_func):
-        """Пример использования: 
-        result = db_manager.execute_query(
-            shard_key='user_123',
-            query_func=lambda session: session.query(User).filter_by(id=123).first()
-        )
+    def _get_node_name(self, shard_key: str) -> str:
+        return self.hash_ring.get_node(shard_key)
+
+
+    @contextmanager
+    def get_session(self, shard_key: str) -> Generator[Session, None, None]:
         """
-        session = self.get_session(shard_key)
+        Контекстный менеджер для получения сессии нужного шарда.
+        Гарантирует commit при успехе и rollback при ошибке.
+        
+        Использование:
+        with db_manager.get_session("user_123") as session:
+            user = session.get(User, "user_123")
+            session.add(new_link)
+        """
+        node_name = self._get_node_name(shard_key)
+        session_factory = self.session_factories[node_name]
+        session = session_factory()
+        
         try:
-            result = query_func(session)
+            yield session
             session.commit()
-            return result
-        except:
+        except Exception:
             session.rollback()
             raise
+        finally:
+            session.close()
